9b236235178f5ea064270dc1c00693929e108eea
MiguelHdez18 <miguel.hernandez@ithealth.co>
2022-08-13 18:09:16 -0500

    firstcommit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 4001;
+const servidor = app.listen(PORT, () => {
+    console.log("server run " + PORT);
+})
